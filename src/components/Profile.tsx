46d2dc065caa35188682a3e967c7f767f69f4b18
Garccosta <garccosta@gmail.com>
2021-02-27 18:58:42 -0300

    Criado Countdown Context, refatorado o cógido e adição de notificação de novos desafios

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
     return (
         <div className= { styles.profileContainer }>
             <img src="https://github.com/Garccosta.png" alt="profile picture" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong> Gabriel Costa </strong>
                 <p> 
                     <img src="icons/level.svg" alt="level"></img>
-                    Level 10
+                    Level { level }
                 </p>
             </div>
         </div>
